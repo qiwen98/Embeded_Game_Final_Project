2b509d69e8cb33e2507cf9eb3d324fb4fdb64849
MrWater <870645367@qq.com>
2020-07-25 07:58:15 +0800

    Camera

--- Embedded_Game_Final/Assets/Script/ThirdPersonInput.cs
+++ Embedded_Game_Final/Assets/Script/ThirdPersonInput.cs
@@ -14,16 +14,34 @@ public class ThirdPersonInput : MonoBehaviour
     protected float CameraAngle;
     protected float CameraAngleSpeed = 0.2f;
 
+    private Vector3 fixVector;
+
+    public float controlY = 1f;
+    public float controlZ = 2f;
+
+    public float dis_ray;
+    public float minControlZ = 1.0f;
+    public float maxControlZ = 3.0f;
+    public float smooth = 10f;
+    
+
     // Use this for initialization
     void Start()
     {
         Control = GetComponent<ThirdPersonUserControl>();
+        controlY = 2.3f;
+        controlZ = 3;
 
+        fixVector = Vector3.down * 0.3f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        
+        
+
         Control.m_Jump = Button.Pressed;
         Control.Hinput = LeftJoystick.inputVector.x;
         Control.Vinput = LeftJoystick.inputVector.y;
@@ -31,8 +49,8 @@ public class ThirdPersonInput : MonoBehaviour
         //CameraAngle += RightJoystick.inputVector.x * CameraAngleSpeed;
         CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
 
-        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0, 3, 2);
-        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+        Camera.main.transform.position = transform.position + fixVector+ Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0f, controlY, controlZ);
+        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + fixVector+ Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
         
     }
 }
